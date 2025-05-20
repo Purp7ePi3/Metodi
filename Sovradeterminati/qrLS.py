3cf41dc42f7a150a6ec4b5aa9fec567661870b3e
Simone <simone.brunelli3@studio.unibo.it>
2025-05-20 22:51:25 +0200

    Prendere e apprendete

---
+++ Sovradeterminati/qrLS.py
@@ -0,0 +1,19 @@
+import numpy as np
+import scipy.linalg as spLin
+import SolveTriangular
+
+def qrLS(A,b):
+#Risolve un sistema sovradeterminato con il metodo QR-LS
+    n=A.shape[1]  # numero di colonne di A
+    Q,R=spLin.qr(A)
+    h = Q.T @ b
+    x,flag=SolveTriangular.Usolve(R[:n, :],h[:n])
+    residuo=np.linalg.norm(h[n:])**2
+    return x,residuo
+
+A = np.array([[1, 1], [1, 2], [1, 3]])
+b = np.array([6, 0, 0])
+
+x, residuo = qrLS(A, b)
+print(f"Soluzione approssimata: {x}")
+print("Residuo:", residuo)
